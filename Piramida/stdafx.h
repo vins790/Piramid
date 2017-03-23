321f2169dc326c2302d24a2c8e8f240c6af256aa
vins790 <insene790@gmail.com>
2017-03-23 18:08:20 +0100

    1.0
    Drawing fractal structure built of piramids.

---
+++ Piramida/stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glut.h>
+#include <cmath>
+#include <iostream>
+#include <Windows.h>
+#include <iostream>
+#include <ctime>
+#define M_PI 3.14159265358979323846
+
+
+
+// TODO: reference additional headers your program requires here
